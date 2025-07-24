c59431b560dc380f2337df4a987599f27f440c31
facundo.bas <facundo.bas@bitlogic.io>
2025-07-24 12:43:55 -0300

    feat(pwa): configuración para instalar como app en móvil

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,28 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
+  pwa: {
+    name: 'MedicApp',
+    themeColor: '#1976d2',
+    msTileColor: '#1976d2',
+    manifestOptions: {
+      short_name: 'MedicApp',
+      start_url: '.',
+      display: 'standalone',
+      background_color: '#ffffff',
+      icons: [
+        {
+          src: './img/97781016-343c-424b-b302-ea7d26d795b0.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: './img/97781016-343c-424b-b302-ea7d26d795b0.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  },
   transpileDependencies: true,
   devServer: {
     proxy: {
@@ -10,4 +33,4 @@ module.exports = defineConfig({
       }
     }
   }
-})
+}
